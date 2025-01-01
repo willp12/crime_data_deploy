6ae8a6720cf4871af68cb64cde5e188734da50cd
willp12 <31415008+willp12@users.noreply.github.com>
2025-01-01 15:15:33 -0300

    feat: adiciona divisores nos headers

--- pages/page_2.py
+++ pages/page_2.py
@@ -14,13 +14,13 @@ teste = teste.rename(columns={'BAIRRO': 'Bairro', 'NUM_BO': 'Número de BOs'})
 
 teste = teste.reset_index(drop=True)
 
-st.header("Top 10 maiores bairros em números de BOs registrados")
+st.header("Top 10 maiores bairros em números de BOs registrados", divider='gray')
 
 st.table(teste)
 
 st.bar_chart(teste, x='Bairro', y='Número de BOs', horizontal=True, x_label='Número de BOs', y_label='Bairros')
 
-st.header("Número de BOs por período do dia")
+st.header("Número de BOs por período do dia", divider=True)
 
 teste2 = pd.pivot_table(crime_table, index='DESCR_PERIODO', values='NUM_BO', aggfunc='count').reset_index()
 
@@ -30,7 +30,7 @@ fig = px.pie(data_frame=teste2, names='Período do Dia', values='Número de BOs'
 
 st.plotly_chart(fig)
 
-st.header("Principais crimes envolvidos")
+st.header("Principais crimes envolvidos", divider=True)
 
 teste3 = pd.pivot_table(crime_table, index='RUBRICA_MOD', values='NUM_BO', aggfunc='count').reset_index()
 
