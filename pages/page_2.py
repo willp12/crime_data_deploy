f0e26bbdd095466b07128bc10d5bcb9d9cc19537
willp12 <31415008+willp12@users.noreply.github.com>
2025-01-01 14:56:45 -0300

    feat: alteracoes em nomes e criacao de cabecalhos

--- pages/page_2.py
+++ pages/page_2.py
@@ -10,20 +10,36 @@ crime_table = database.retorna_query('data/crime_data.duckdb', database.ler_quer
 
 teste = pd.pivot_table(crime_table, index='BAIRRO', values='NUM_BO', aggfunc='count').reset_index().sort_values(by='NUM_BO', ascending=False)[:10]
 
+teste = teste.rename(columns={'BAIRRO': 'Bairro', 'NUM_BO': 'Número de BOs'})
+
+teste = teste.reset_index(drop=True)
+
+print(teste)
+
+st.header("Top 10 maiores bairros em números de BOs registrados")
+
 st.table(teste)
 
-st.bar_chart(teste, x='BAIRRO', y='NUM_BO', horizontal=True, x_label='Número de BOs', y_label='Bairros')
+st.bar_chart(teste, x='Bairro', y='Número de BOs', horizontal=True, x_label='Número de BOs', y_label='Bairros')
+
+st.header("Número de BOs por período do dia")
 
 teste2 = pd.pivot_table(crime_table, index='DESCR_PERIODO', values='NUM_BO', aggfunc='count').reset_index()
 
-fig = px.pie(data_frame=teste2, names='DESCR_PERIODO', values='NUM_BO', hole=0.3)
+teste2 = teste2.rename(columns={'DESCR_PERIODO': 'Período do Dia', 'NUM_BO': 'Número de BOs'})
+
+fig = px.pie(data_frame=teste2, names='Período do Dia', values='Número de BOs', hole=0.3)
 
 st.plotly_chart(fig)
 
+st.header("Principais crimes envolvidos")
+
 teste3 = pd.pivot_table(crime_table, index='RUBRICA_MOD', values='NUM_BO', aggfunc='count').reset_index()
+
+teste3 = teste3.rename(columns={'RUBRICA_MOD': 'Tipo de Crime', 'NUM_BO': 'Número de BOs'})
 
 st.table(teste3)
 
-fig3 = px.pie(data_frame=teste3, names='RUBRICA_MOD', values='NUM_BO', hole=0.3)
+fig3 = px.pie(data_frame=teste3, names='Tipo de Crime', values='Número de BOs', hole=0.3)
 
 st.plotly_chart(fig3)
